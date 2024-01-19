166adb27a7b6e0c75e29f59d830c7ba04d8eb5e5
xoventechdev <alkarim.doha@gmail.com>
2024-01-19 15:32:47 +0300

    removed error

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-
- 
-
-
     var json = {
         "name": "Hosen",
         "age": 21,
@@ -13,13 +9,4 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(json));
 
-
-    
-
-
-
-}).listen(5050, function () {
-    console.log('Server is listening on port 4040');
-}).on('error', function (err) {
-    console.error('Error starting server:', err.message);
-});
+}).listen(5050);
