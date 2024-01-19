aeffd9eca0885d137f182e2d29ddd2fff0c74378
xoventechdev <alkarim.doha@gmail.com>
2024-01-19 15:21:02 +0300

    Location Search

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,11 @@
-const aipurl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric';
+const aipurl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=96ec9cc2dda24013e1ac1a45d4b7aa1b&units=metric';
 let body = document.querySelector('p');
+let serachClick = document.querySelector('#searchCity');
+
 
 async function getWeather(city) {
-    const response = await fetch(aipurl.replace('{city name}', city).replace('{API key}', '96ec9cc2dda24013e1ac1a45d4b7aa1b'));
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=96ec9cc2dda24013e1ac1a45d4b7aa1b&units=metric`);
     const data = await response.json();
-
-
     
     body.innerHTML = `<span class="data">
     <span id="city">${data.name}</span>
@@ -15,3 +15,8 @@ async function getWeather(city) {
 }
 
 getWeather('Doha');
+serachClick.addEventListener('click', function () {
+    let city = document.querySelector('#cityName').value;
+    getWeather(city);
+});
+
